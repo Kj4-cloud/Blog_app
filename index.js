975668a56479b26aa8b841ae9fd279c52de6b595
Karan Jadhav <karanjadhav041006@gmail.com>
2026-06-28 20:44:53 +0530

    Blog adding Done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const userRouter = require('./routes/user');
+const blogRouter = require('./routes/blog');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 
+
+const Blog = require('./model/blog');
 
 //connect to mongodb
 mongoose.connect('mongodb://localhost:27017/blogging_app')
@@ -26,14 +29,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie('token'));
+app.use(express.static(path.resolve('./public')));
 
-app.get('/', (req,res) =>{  
+
+app.get('/', async (req, res) => {
+    const allBlogs = await Blog.find().sort('createdBy').sort({ createdAt: -1 });
     res.render('home', {
         user: req.user,
+        blogs: allBlogs
     });
 })
 
 app.use('/user',userRouter);
+
+app.use('/blog',blogRouter);
+
 
 app.listen(PORT , () =>{
     console.log(`Server is running on port ${PORT}`);
